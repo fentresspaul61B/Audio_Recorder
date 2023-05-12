a65da53ed2fde4dc0903b65dbba7de0ac4db93db
fentresspaul61B <77761564+fentresspaul61B@users.noreply.github.com>
2023-05-11 17:17:25 -0700

    got it working.

---
+++ UI.py
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:6c57ba27b267dc1e1e7a318839190ddef35bcd0d67897bac4a5d45328817fc0a
+size 1131
---
+++ make_inference.py
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:3c1aba70f9548b5cad3036cb6e5b07a96d9b7177a8ffb07292641f1b8025ac8e
+size 542
